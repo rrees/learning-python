b7a69e234ac9feba6387ad100705526cb08b592f
Robert Rees <robert.rees@guardian.co.uk>
2014-12-25 22:19:47 +0000

    Adds Boolean literals and an integer case to make promotion clearer

--- test_datatypes.py
+++ test_datatypes.py
@@ -2,5 +2,15 @@ import unittest
 
 class TestDataTypes(unittest.TestCase):
 
+    def test_integer_arithmetic(self):
+        self.assertEqual(3 + 4, 7)
+        
     def test_number_promotion(self):
         self.assertEqual(3.0 + 4, 7.0)
+
+    def test_string_concatenation(self):
+        self.assertEqual("hello " + "world", "hello world")
+
+    def test_boolean_literals(self):
+        self.assertTrue(True)
+        self.assertFalse(False)
