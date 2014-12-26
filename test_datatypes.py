86f1345477594c24f7ecc5ca5f58f07b8a1092c2
Robert Rees <robert.rees@guardian.co.uk>
2014-12-26 14:27:47 +0000

    Adds start of generators code and also starts to descibe the collection objects

--- test_datatypes.py
+++ test_datatypes.py
@@ -4,7 +4,7 @@ class TestDataTypes(unittest.TestCase):
 
     def test_integer_arithmetic(self):
         self.assertEqual(3 + 4, 7)
-        
+
     def test_number_promotion(self):
         self.assertEqual(3.0 + 4, 7.0)
 
@@ -14,3 +14,13 @@ class TestDataTypes(unittest.TestCase):
     def test_boolean_literals(self):
         self.assertTrue(True)
         self.assertFalse(False)
+
+class TestCollections(unittest.TestCase):
+    def test_sets(self):
+        spam_set = set(['spam', 'spam', 'spam'])
+        self.assertEqual(len(spam_set), 1)
+
+    def test_array_index_access(self):
+        breakfast = ['spam', 'beans', 'eggs']
+        self.assertEqual(breakfast[0], 'spam')
+        self.assertEqual(breakfast[2], 'eggs')
--- test_generators.py
+++ test_generators.py
@@ -2,4 +2,5 @@ import unittest
 
 class TestGeneratorCode(unittest.TestCase):
     def test_range(self):
+        # Range generates numbers starting at zero to match Python's indexing
         self.assertEqual(range(3), [0,1,2])
