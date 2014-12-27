d9e80fbb106ca07f7edbe220536966ac5e7b51ff
Robert Rees <robert.rees@guardian.co.uk>
2014-12-27 10:36:27 +0000

    Adds the start of the generator exercise

---
+++ generators.py
@@ -0,0 +1,8 @@
+
+def generate_numbers(upper_limit):
+    # Implement this via the range in-built function
+    pass
+
+def generate_natural_numbers(upper_limit):
+    # Implement this in terms of changing the output of generate_numbers
+    pass
--- test_generators.py
+++ test_generators.py
@@ -1,6 +1,18 @@
 import unittest
 
+import generators
+
 class TestGeneratorCode(unittest.TestCase):
     def test_range(self):
         # Range generates numbers starting at zero to match Python's indexing
         self.assertEqual(range(3), [0,1,2])
+
+    @unittest.skip("implement generators module")
+    def test_creating_numbers(self):
+        self.assertSequenceEqual(generators.generate_numbers(10),
+            [0,1,2,3,4,5,6,7,8,9])
+
+    @unittest.skip("implement generators module")
+    def test_creating_natural_numbers(self):
+        self.assertSequenceEqual(generators.generate_natural_numbers(10),
+            [1,2,3,4,5,6,7,8,9,10])
