f43183813a349611be838880b12e7310be44c3f0
Robert Rees <robert.rees@guardian.co.uk>
2014-12-30 11:35:52 +0000

    Adds more tests to illustrate collections and boolean truthiness

--- test_datatypes.py
+++ test_datatypes.py
@@ -20,7 +20,35 @@ class TestCollections(unittest.TestCase):
         spam_set = set(['spam', 'spam', 'spam'])
         self.assertEqual(len(spam_set), 1)
 
-    def test_array_index_access(self):
+    def test_lists(self):
+        spam_list = ['spam', 'spam', 'spam']
+        self.assertEqual(len(spam_list), 3)
+
+    def test_list_index_access(self):
         breakfast = ['spam', 'beans', 'eggs']
+        self.assertEqual(len(breakfast), 3)
         self.assertEqual(breakfast[0], 'spam')
         self.assertEqual(breakfast[2], 'eggs')
+
+    def test_list_append(self):
+        breakfast = []
+        breakfast.append('spam')
+        self.assertEqual(len(breakfast), 1)
+
+    def test_dictionary_addition(self):
+        menu = {'Spam and eggs' : ['spam', 'eggs']}
+        self.assertEqual(len(menu.keys()), 1)
+        menu['Spam, bacon and eggs'] = ['spam', 'bacon', 'eggs']
+
+class TestBooleanEvaluation(unittest.TestCase):
+    def test_numbers(self):
+        self.assertFalse(0)
+        self.assertTrue(7)
+
+    def test_lists(self):
+        self.assertFalse([])
+        self.assertTrue([1])
+
+    def test_dictionaries(self):
+        self.assertFalse({})
+        self.assertTrue({'hello': True})
