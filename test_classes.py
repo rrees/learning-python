f968fa99396593f440f9130824cf177096508c4b
Robert Rees <shudderfix@gmail.com>
2015-01-01 15:26:11 +0000

    Adds tuple tests and some more WordCounter class examples

--- test_classes.py
+++ test_classes.py
@@ -8,3 +8,17 @@ class TestWordCounter(unittest.TestCase):
         word_counter = WordCounter()
         word_counter.count("polly")
         word_counter.count("parrot")
+
+        self.assertEquals(word_counter.count(), 2)
+        self.assertEquals(word_counter.count(word="polly"), 1)
+        self.assertEquals(word_counter.count(word="parrot"), 1)
+
+    @unittest.skip("check that the count tracks dupes correctly")
+    def test_word_counter(self):
+        word_counter = WordCounter()
+
+        for i in range(3):
+            word_counter.count("spam")
+
+        self.assertEquals(word_counter.count(), 3)
+        self.assertEquals(word_counter.count(word="spam"), 3)
