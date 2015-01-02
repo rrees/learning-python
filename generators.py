dd9b9f558c9fc4e86af9c0f7c5d371c33bf7476e
Robert Rees <shudderfix@gmail.com>
2015-01-02 14:17:56 +0000

    Working generator example based on Fibonacci

--- generators.py
+++ generators.py
@@ -1,1 +1,6 @@
 
+def fib():
+    a, b = 0,1
+    while True:
+        yield a
+        a,b = b, a+b
--- test_generators.py
+++ test_generators.py
@@ -1,4 +1,9 @@
 import unittest
 
+import generators
+
 class TestGenerators(unittest.TestCase):
-    pass
+    def test_fib_generation(self):
+        fib = generators.fib()
+        first_fibs = [fib.next() for i in range(3)]
+        self.assertSequenceEqual(first_fibs, [0,1,1])
