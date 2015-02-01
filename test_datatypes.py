9678f4ee8849799656d16b17146afaf19b207ea6
Robert Rees <shudderfix@gmail.com>
2015-02-01 10:48:46 +0000

    Separates and reorganises the tests based on the first lesson

--- test_datatypes.py
+++ test_datatypes.py
@@ -15,6 +15,22 @@ class TestDataTypes(unittest.TestCase):
         self.assertTrue(True)
         self.assertFalse(False)
 
+class TestBooleanEvaluation(unittest.TestCase):
+    def test_numbers(self):
+        self.assertFalse(0)
+        self.assertTrue(7)
+
+    def test_lists(self):
+        self.assertFalse([])
+        self.assertTrue([1])
+
+    def test_dictionaries(self):
+        self.assertFalse({})
+        self.assertTrue({'hello': True})
+
+    def test_none_is_false(self):
+        pass
+
 class TestCollections(unittest.TestCase):
     def test_sets(self):
         spam_set = set(['spam', 'spam', 'spam'])
@@ -24,6 +40,7 @@ class TestCollections(unittest.TestCase):
         spam_list = ['spam', 'spam', 'spam']
         self.assertEqual(len(spam_list), 3)
 
+class TestDictionaries:
     def test_dictionary_addition(self):
         menu = {'Spam and eggs' : ['spam', 'eggs']}
         self.assertEqual(len(menu.keys()), 1)
@@ -38,6 +55,20 @@ class TestCollections(unittest.TestCase):
         menu = {'Spam and eggs' : ['spam', 'eggs']}
         del(menu['Spam and eggs'])
         self.assertEqual(len(menu.keys()), 0)
+
+class TestSets(unittest.TestCase):
+    def test_function_versus_syntax_sugar(self):
+        self.assertEqual({1,2,3}, set([1,2,3]))
+
+    def test_set_operations(self):
+        ints = {1,2,3,4,5}
+        evens = {2,4}
+
+        self.assertItemsEqual({2,4}, ints.intersection(evens))
+        self.assertItemsEqual({1,3,5}, ints.difference(evens))
+
+        self.assertTrue(ints.issuperset(evens))
+        self.assertTrue(evens.issubset(ints))
 
 class TestLists(unittest.TestCase):
     def test_list_index_access(self):
@@ -61,16 +92,3 @@ class TestTuples(unittest.TestCase):
         self.assertEqual(x, 3)
         self.assertEqual(y, 4)
         self.assertEqual(z, 5)
-
-class TestBooleanEvaluation(unittest.TestCase):
-    def test_numbers(self):
-        self.assertFalse(0)
-        self.assertTrue(7)
-
-    def test_lists(self):
-        self.assertFalse([])
-        self.assertTrue([1])
-
-    def test_dictionaries(self):
-        self.assertFalse({})
-        self.assertTrue({'hello': True})
