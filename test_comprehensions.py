3b940ce1c553ffd6ac0a49b1c6a1f3d42efa6edf
Robert Rees <shudderfix@gmail.com>
2015-02-07 09:46:46 +0000

    Basic syntax for all the generators demonstrated

--- test_comprehensions.py
+++ test_comprehensions.py
@@ -7,6 +7,16 @@ class TestGeneratorCode(unittest.TestCase):
         # Range generates numbers starting at zero to match Python's indexing
         self.assertEqual(range(3), [0,1,2])
 
+    def test_generator_syntax(self):
+        self.assertSequenceEqual([0, 2, 4, 6], [i * 2 for i in range(4)])
+
+    def test_set_generator_syntax(self):
+        self.assertSequenceEqual({1}, {i for i in [1,1,1]})
+
+    def test_dictionary_generator_syntax(self):
+        self.assertDictEqual({"a" : 2, "b": 3}, {key: value + 1 for key, value in {"a": 1, "b": 2}.items()})
+
+class TestCreatingGenerators(unittest.TestCase):
     @unittest.skip("implement comprehensions module")
     def test_creating_numbers(self):
         self.assertSequenceEqual(comprehensions.generate_numbers(10),
