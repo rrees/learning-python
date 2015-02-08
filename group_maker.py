aff58e5ea922d9268db03a37dfb0bb8be8ee7d9a
Robert Rees <shudderfix@gmail.com>
2015-02-08 22:30:05 +0000

    Adds a few comments about what is wrong with the group assiger

--- group_maker.py
+++ group_maker.py
@@ -2,6 +2,12 @@ import random
 import itertools
 import pprint
 
+# Issues to fix with this program
+
+# Groups should be of the largest equal size
+# Any people left over should be spread across the groups
+# I.e. no-one left in a small group or just by themselves
+
 people =[
 
 ]
@@ -13,7 +19,7 @@ def chunker(itr, size):
 		item = list(itertools.islice(itr, size))
 
 if __name__ == '__main__':
-	no_of_groups = 3
+	no_of_groups = 3 # needs to be calculated
 	random.shuffle(people)
 	groups = [g for g in chunker(iter(people), len(people)/no_of_groups)]
 	pprint.pprint(groups)
