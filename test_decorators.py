f99305e29a3916e1ddd8ce041ecc35960274b77a
Robert Rees <robert.rees@guardian.co.uk>
2015-03-05 11:01:19 +0000

    Adds a stub test to the decorator unit tests

--- test_decorators.py
+++ test_decorators.py
@@ -5,3 +5,6 @@ import decorators
 class TestDecoration(unittest.TestCase):
     def test_upper_case_example(self):
         self.assertEquals(decorators.greet("world"), 'HELLO WORLD')
+
+    def test_lower_case_example(self):
+    	pass
