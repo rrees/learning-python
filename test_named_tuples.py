f614bd2aadcea036c208a46d58718e4c46d02ea8
Robert Rees <shudderfix@gmail.com>
2015-03-08 11:14:23 +0000

    Adds a test around named tuples

---
+++ test_named_tuples.py
@@ -0,0 +1,21 @@
+import unittest
+
+from collections import namedtuple
+
+Point = namedtuple('Point', ['x', 'y'])
+
+
+
+class TestNamedTuples(unittest.TestCase):
+    def test_named_tuples(self):
+    	p = Point(2,3)
+
+    	self.assertEqual(p.x, 2)
+    	self.assertEqual(p.y, 3)
+
+    def test_named_tuples_are_immutable(self):
+
+    	p = Point(5, 8)
+
+        with self.assertRaises(AttributeError):
+            p.b = 3
