f6d1c302eeeb5a0cb1b81f15a4762bad81490b58
Robert Rees <shudderfix@gmail.com>
2015-03-08 11:20:49 +0000

    Examples of hasattr

---
+++ test_reflection.py
@@ -0,0 +1,23 @@
+import unittest
+
+from collections import namedtuple
+
+Point = namedtuple('Point', ['x', 'y'])
+
+class PointClass:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+class TestReflection(unittest.TestCase):
+	def setUp(self):
+		self.p = Point(9, 15)
+		self.pc = PointClass(23, 4)
+		self.all = [self.p, self.pc]
+
+	def test_attribute_checking(self):
+		for item in self.all:
+			self.assertTrue(hasattr(item, 'x'))
+			self.assertTrue(hasattr(item, 'y'))
+
+			self.assertFalse(hasattr(item, 'b'))
