dc5eb8b5adf84c299da218ddb00e2bd5e8e34a6f
Robert Rees <robert.rees@guardian.co.uk>
2015-03-13 23:37:56 +0000

    Adds a long note on how dictionaries work

--- classes.py
+++ classes.py
@@ -8,3 +8,17 @@ class WordCounter:
 
 	def count(self, word=None):
 		pass
+
+'''
+A reminder on how dictionaries work...
+
+Create a dictionary: d = {}
+
+Given a dictionary d, a key k and a value v:
+
+To read a value d[k] or d.get(k, default_value)
+To set a value in d: d[k] = v
+
+To get all the keys in a dictionary: d.keys()
+To get all the values in a dictionary: d.values()
+'''
