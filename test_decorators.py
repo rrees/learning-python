807db7b03c10b048ef534ee0b783fa50508614ff
Robert Rees <robert.rees@guardian.co.uk>
2015-03-16 13:50:50 +0000

    Makes the stub function more explicit in the decorators example

--- test_decorators.py
+++ test_decorators.py
@@ -6,5 +6,6 @@ class TestDecoration(unittest.TestCase):
     def test_upper_case_example(self):
         self.assertEquals(decorators.greet("world"), 'HELLO WORLD')
 
+    @unittest.skip('Implement the lowercase decorator first')
     def test_lower_case_example(self):
-    	pass
+        self.assertEquals(decorators.farewell("world"), 'farewell world')
